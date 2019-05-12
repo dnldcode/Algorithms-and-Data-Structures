c19ca3d0926b4a659413bc90ffc4afb825fc17ee
dnldcode <dnldcode@gmail.com>
2019-05-12 18:29:17 +0200

    Coin change problem

---
+++ Project 2 - Coin change problem/Code(C#)/Project2/CoinChange.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+	static class CoinChange
+	{
+		public static string MakeChange(int change)
+		{
+			if (change < 0)
+				throw new Exception("Input should be a positive integer");
+
+			int[] coins = new int[7] { 1, 2, 5, 10, 20, 50, 100 };
+
+			Array.Sort(coins);
+			Array.Reverse(coins);
+
+			string res = "";
+			foreach (int coin in coins)
+			{
+				int howMany = change / coin;
+				change = change % coin;
+				if (howMany != 0)
+					res += String.Format($"{howMany}x{coin}\n");
+			}
+			return res;
+		}
+
+		public static void MakeChange(int[] arrCoins, int change)
+		{
+			if (change < 0)
+				throw new Exception("Input should be a positive integer");
+
+			foreach (int coin in arrCoins)
+			{
+				if (coin < 0)
+					throw new Exception("Coin should be positive integer");
+			}
+
+			if (arrCoins.Length != arrCoins.Distinct().Count())
+				throw new Exception("Coins should have unique values");
+
+			if (!arrCoins.Contains(1))
+				throw new Exception("One coin must have value of 1");
+
+			int[] coins = new int[arrCoins.Length];
+			Array.Copy(arrCoins, coins, arrCoins.Length);
+			Array.Sort(coins);
+			Array.Reverse(coins);
+
+			List<string> changeList = new List<string>();
+			foreach (int coin in coins)
+			{
+				int howMany = change / coin;
+				change = change % coin;
+				if (howMany != 0)
+					changeList.Add(String.Format($"{howMany}x{coin}"));
+			}
+
+			Console.WriteLine(string.Join(", ", changeList));
+		}
+
+		public static void MakeChangeDynamic(int[] arrCoins, int change)
+		{
+			if (change < 0)
+				throw new Exception("Input should be a positive integer");
+
+			foreach (int coin in arrCoins)
+			{
+				if (coin < 0)
+					throw new Exception("Coin should be positive integer");
+			}
+
+			if (arrCoins.Length != arrCoins.Distinct().Count())
+				throw new Exception("Coins should have unique values");
+
+			if (!arrCoins.Contains(1))
+				throw new Exception("One coin must have value of 1");
+
+			int[] coins = new int[arrCoins.Length];
+			Array.Copy(arrCoins, coins, arrCoins.Length);
+			Array.Sort(coins);
+
+			int[] minCoins = new int[change + 1];
+			int[] firstCoinIndex = new int[change + 1];
+
+			for (int currChange = 0; currChange < change + 1; currChange++)
+			{
+				int coinCount = currChange;
+				int newCoinIndex = 0;
+
+				for (int coinIndex = 0; coinIndex < coins.Length; coinIndex++)
+				{
+					int coin = coins[coinIndex];
+					if (coin > currChange)
+						continue;
+					if (1 + minCoins[currChange - coin] < coinCount)
+					{
+						coinCount = 1 + minCoins[currChange - coin];
+						newCoinIndex = coinIndex;
+					}
+				}
+				minCoins[currChange] = coinCount;
+				firstCoinIndex[currChange] = newCoinIndex;
+			                              }
+
+			int currChange2 = change;
+			int[] coincount = new int[arrCoins.Length];
+			List<string> changeList = new List<string>();
+
+			while (currChange2 > 0)
+			{
+				int coin = coins[firstCoinIndex[currChange2]];
+				coincount[firstCoinIndex[currChange2]]++;
+				currChange2 -= coin;
+			}
+			for (int i = 0; i < coincount.Length; i++)
+			{
+				if (coincount[i] > 0)
+					changeList.Add(coincount[i] + "x" + coins[i]);
+			}
+			changeList.Reverse();
+			Console.WriteLine(string.Join(", ", changeList));
+
+		}
+	}
+}
---
+++ Project 2 - Coin change problem/Code(C#)/Project2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CoinChange.MakeChangeDynamic(new int[] { 1, 3, 4 }, 6);
+
+			Stopwatch stopwatch = new Stopwatch();
+
+			for (int i = 10; i <= 20; i++)
+			{
+				int length = (int)Math.Pow(2, i);
+				int[] coins = new int[length];
+
+				coins[0] = 1;
+				for (int j = 1; j < length; j++)
+					coins[j] = (j + 1) * 2;
+
+				stopwatch.Restart();
+				Console.WriteLine($"Greedy algorithm for general case(100) coins = {length}:\n");
+				CoinChange.MakeChange(coins, 100);
+				stopwatch.Stop();
+				Console.WriteLine("It took " + stopwatch.Elapsed.TotalMilliseconds);
+
+				stopwatch.Restart();
+				Console.WriteLine($"Dynamic solution for general case(100) coins = {length}:\n");
+				CoinChange.MakeChangeDynamic(coins, 100);
+				stopwatch.Stop();
+				Console.WriteLine("Time: " + stopwatch.Elapsed.TotalMilliseconds);
+				Console.WriteLine("----------------------------------");
+			}
+
+			for (int i = 10; i <= 20; i++)
+			{
+				int[] coins = new int[7] { 1, 2, 5, 10, 20, 50, 100 };
+
+				int change = (int)Math.Pow(2, i);
+
+				stopwatch.Restart();
+				Console.WriteLine($"Greedy algorithm for general case(100) change = {change}:\n");
+				CoinChange.MakeChange(coins, change);
+				stopwatch.Stop();
+				Console.WriteLine("It took " + stopwatch.Elapsed.TotalMilliseconds);
+
+				stopwatch.Restart();
+				Console.WriteLine($"\nCoin change for general case(100) change = {change}:\n");
+				CoinChange.MakeChangeDynamic(coins, change);
+				stopwatch.Stop();
+				Console.WriteLine("Time: " + stopwatch.Elapsed.TotalMilliseconds);
+				Console.WriteLine("----------------------------------");
+			}
+		}
+	}
+}
